082ed3dd14881168705e9d84489ec5a0971f33e4
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-03-03 11:29:47 +0700

    Add test project

---
+++ AutoDummyDemo/AutoDummyDemo/AutoDummyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoDummyDemoApp.swift
+//  AutoDummyDemo
+//
+//  Created by Bliss on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoDummyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoDummyDemo/AutoDummyDemo/NewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewProtocol.swift
+//  AutoDummyDemo
+//
+//  Created by Bliss on 3/3/23.
+//
+
+import Foundation
